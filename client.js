45722b18db99fcfe1e486b56de2c0985266a9547
DiartGerxhaliu <diart.gerxhaliu@student.uni-pr.edu>
2026-04-18 14:41:57 +0200

    Pjesa e 2te krijimi i konneksionit ids sendit, ndarja e argumenteve, he dergimi i mesazheve

--- client.js
+++ client.js
@@ -16,3 +16,35 @@ const serverPort = Number(getArg('--port', String(DEFAULT_UDP_PORT)));
 const socket = dgram.createSocket('udp4');
 
 let currentRole = 'reader';
+
+function getArg(flag, fallback) {
+  const index = args.indexOf(flag);
+  return index >= 0 && args[index + 1] ? args[index + 1] : fallback;
+}
+
+function send(packet) {
+  socket.send(Buffer.from(JSON.stringify(packet)), serverPort, serverHost);
+}
+
+function newId() {
+  return crypto.randomUUID();
+}
+
+function connectMsg() {
+  send({
+    type: 'connectMsg',
+    clientId,
+    name: clientName,
+    wantAdmin,
+    token,
+  });
+}
+
+function sendText(text) {
+  send({
+    type: 'text',
+    clientId,
+    requestId: newId(),
+    text,
+  });
+}
